3e7dbb7631d4aa7455009a5afd382eeb2a2af315
Jhoselin Guachizaca <jaguachizaca@gmail.com>
2019-10-24 15:36:51 -0500

    Ejercicio3

---
+++ Ejercicio03/src/ejercicio03/Ejercicio03.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio03;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Acer
+ */
+public class Ejercicio03 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner entrada = new Scanner(System.in); //un objeto tipo Scanner
+        String nombre;
+        String universidad;
+        
+        nombre = "Jhoselin Guachizaca";
+        universidad = "UTPL";
+        
+        System.out.print("Mi nombre es\n\t"+nombre+"\n"+universidad+"\n");
+        
+    }
+    
+}
